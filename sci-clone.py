b971b99a7133cc2ac890741c362cecc29b878df0
LI Fumin <lifuminster@gmail.com>
2021-08-15 17:00:52 +0800

    update

--- sci-clone.py
+++ sci-clone.py
@@ -46,15 +46,11 @@ else:
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 
 def setup_logger(name, log_file, level=logging.INFO):
-    """To setup as many loggers as you want"""
-
     handler = logging.FileHandler(log_file, mode='w')
     handler.setFormatter(formatter)
-
     logger = logging.getLogger(name)
     logger.setLevel(level)
     logger.addHandler(handler)
-
     return logger
 
 
