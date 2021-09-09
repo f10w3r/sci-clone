dd41b7a8e6e9e719f9908a594584aae030d6b353
LI Fumin <lifuminster@gmail.com>
2021-09-09 11:55:23 +0800

    update

--- sci-clone.py
+++ sci-clone.py
@@ -8,17 +8,9 @@ from requests.compat import urljoin
 from pyfiglet import Figlet
 
 class color:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   ITALIC = '\033[3m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
+   PURPLE = '\033[95m'; CYAN = '\033[96m'; DARKCYAN = '\033[36m'; BLUE = '\033[94m'
+   GREEN = '\033[92m'; YELLOW = '\033[93m'; RED = '\033[91m'
+   BOLD = '\033[1m'; ITALIC = '\033[3m'; UNDERLINE = '\033[4m'; END = '\033[0m'
 
 def setup_logger(name, log_file, level=logging.INFO):
     # logging format
